2b53b7c6bd94213f432b2d10426e36574e0a510a
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-07-11 22:17:51 -0300

    Global styles and background styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './routes'
 
+import GlobalStyle from './styles/globals'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   )
 }
