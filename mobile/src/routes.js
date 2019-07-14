fcce9433b29f0c1e886f19371757ba8d5f9fc6f5
Emilio Schaedler Heinzmann <emilio.heinzmann@cwi.com.br>
2019-07-14 10:25:22 -0300

    Navigation bewttwen pages on mobile

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createAppContainer, createStackNavigator } from 'react-navigation'
-import { Image } from 'react-native'
+import { Image, TouchableOpacity } from 'react-native'
 
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import logo from './assets/images/logo.png'
@@ -17,14 +17,33 @@ const Routes = createAppContainer(
     {
       headerLayoutPreset: 'left',
       headerBackTitleVisible: false,
-      defaultNavigationOptions: {
+      defaultNavigationOptions: ({ navigation }) => ({
         headerStyle: {
           backgroundColor: '#141419',
         },
         headerTintColor: '#FFF',
-        headerLeft: <Image source={logo} style={{ marginLeft: 10 }} />,
-        headerRight: <Icon name='shopping-cart' size={24} color='#FFF' style={{ marginRight: 10 }} />
-      },
+        headerLeft: (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Home')}
+            activeOpacity={0.8}
+          >
+            <Image source={logo} style={{ marginLeft: 10 }} />
+          </TouchableOpacity>
+        ),
+        headerRight: (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Cart')}
+            activeOpacity={0.8}
+          >
+            <Icon
+              name='shopping-cart'
+              size={25}
+              color='#FFF'
+              style={{ marginRight: 10 }}
+            />
+          </TouchableOpacity>
+        ),
+      }),
     }
   )
 )
